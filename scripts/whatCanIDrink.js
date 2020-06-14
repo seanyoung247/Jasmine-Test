c7a09a76cd9d49654e655fe337a3386b7d0c6e80
Sean Young <seanyoung247@gmail.com>
2020-06-14 13:52:24 +0000

    Added tests to complete whatCanIDrink() spec

--- scripts/whatCanIDrink.js
+++ scripts/whatCanIDrink.js
@@ -2,8 +2,11 @@ function whatCanIDrink(age) {
     var ret = "This is impossible!"
     if (typeof(age) != "number" || age < 0) {
         ret = "Sorry. I can’t tell what drink because that age is incorrect!"
-    } else if (age < 14) {
+    } else if (age >= 0 && age < 14) {
         ret = "Drink Toddy";
+    } else {
+        //Catch all, should happen, but that doesn't mean it won't...
+        ret = "Sorry. I can’t tell what drink because that age is incorrect!"
     }
     return ret;
 }
--- spec/whatCanIDrinkSpec.js
+++ spec/whatCanIDrinkSpec.js
@@ -14,13 +14,31 @@ describe("whatCanIDrink", function() {
     });
 
     //age is less than 18 (top extent)
+    it ("should return: \"Drink Coke\"", function() {
+        expect(whatCanIDrink(17)).toBe("Drink Coke");
+    });
     //age is less than 18 (bottom extent)
+    it ("should return: \"Drink Coke\"", function() {
+        expect(whatCanIDrink(14)).toBe("Drink Coke");
+    });
 
     //age is less than 21 (top extent)
+    it ("should return: \"Drink Beer\"", function() {
+        expect(whatCanIDrink(20)).toBe("Drink Beer");
+    });
     //age is less than 21 (bottom extent)
+    it ("should return: \"Drink Beer\"", function() {
+        expect(whatCanIDrink(18)).toBe("Drink Beer");
+    });
 
     //age is less than 130 (top extent)
+    it ("should return: \"Drink Whisky\"", function() {
+        expect(whatCanIDrink(129)).toBe("Drink Whisky");
+    });
     //age is less than 130 (bottom extent)
+    it ("should return: \"Drink Whisky\"", function() {
+        expect(whatCanIDrink(21)).toBe("Drink Whisky");
+    });
 
     //non number input
     it("should return: \"Sorry. I can’t tell what drink because that age is incorrect!\"", function() {
