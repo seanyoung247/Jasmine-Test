1db646f87df493a8a54d9997dd05c2bb2899c1e6
Sean Young <seanyoung247@gmail.com>
2020-06-14 13:34:04 +0000

    Added <14 test
    Added tests for whatCanIDrink() function for age <14 case.

--- spec/whatCanIDrinkSpec.js
+++ spec/whatCanIDrinkSpec.js
@@ -2,12 +2,22 @@ describe("whatCanIDrink", function() {
     //age is less than 0
     
     it("should return: \"Sorry. I can’t tell what drink because that age is incorrect!\"", function() {
-        expect(whatCanIDrink(-4)).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
+        expect(whatCanIDrink(-1)).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
     });
-    //age is less than 14
-    //age is less than 18
-    //age is less than 21
-    //age is less than 30
+    //age is less than 14 (top extent)
+    it ("should return: \"Drink Toddy\"", function() {
+        expect(whatCanIDrink(13)).toBe("Drink Toddy");
+    });
+    //age is less than 14 (bottom extent)
+    it ("should return: \"Drink Toddy\"", function() {
+        expect(whatCanIDrink(0)).toBe("Drink Toddy");
+    });
+    //age is less than 18 (top extent)
+    //age is less than 18 (bottom extent)
+    //age is less than 21 (top extent)
+    //age is less than 21 (bottom extent)
+    //age is less than 130 (top extent)
+    //age is less than 130 (bottom extent)
     //non number input
     it("should return: \"Sorry. I can’t tell what drink because that age is incorrect!\"", function() {
         expect(whatCanIDrink("Older than you")).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
