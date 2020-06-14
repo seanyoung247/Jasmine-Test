0347e53b0d01e0003002ab16e4bfc60f190eeeaf
Sean Young <seanyoung247@gmail.com>
2020-06-14 13:57:22 +0000

    Completed whatCanIDrink()
    Added code to satisfy testing spec to whatCanIDrink.js.

--- scripts/whatCanIDrink.js
+++ scripts/whatCanIDrink.js
@@ -4,6 +4,12 @@ function whatCanIDrink(age) {
         ret = "Sorry. I can’t tell what drink because that age is incorrect!"
     } else if (age >= 0 && age < 14) {
         ret = "Drink Toddy";
+    } else if (age >= 14 && age < 18) {
+        ret = "Drink Coke";
+    } else if (age >= 18 && age < 21) {
+        ret = "Drink Beer";
+    } else if (age >= 21 && age < 130) {
+        ret = "Drink Whisky";
     } else {
         //Catch all, should happen, but that doesn't mean it won't...
         ret = "Sorry. I can’t tell what drink because that age is incorrect!"
